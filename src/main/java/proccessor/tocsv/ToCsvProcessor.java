fc43af550973bfcfad62f001b6a7483a631ac867
ESeokhwan <huan99@hanmail.net>
2024-07-01 18:08:02 +0900

    feat: add skeleton csv processor

---
+++ src/main/java/proccessor/tocsv/ToCsvProcessor.java
@@ -0,0 +1,12 @@
+package proccessor.tocsv;
+
+import core.Comment;
+import core.ICommentProcessor;
+
+public class ToCsvProcessor implements ICommentProcessor {
+
+  @Override
+  public void commitData(Comment comment) {
+    // do something
+  }
+}
